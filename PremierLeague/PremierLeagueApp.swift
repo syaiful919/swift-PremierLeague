9457cccaecde5f98b9e3b9379404198ff8b1c204
syaiful919 <syaiful919@gmail.com>
2021-06-02 09:39:02 +0700

    Initial Commit

---
+++ PremierLeague/PremierLeagueApp.swift
@@ -0,0 +1,17 @@
+//
+//  PremierLeagueApp.swift
+//  PremierLeague
+//
+//  Created by Syaiful Salam on 02/06/21.
+//
+
+import SwiftUI
+
+@main
+struct PremierLeagueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
